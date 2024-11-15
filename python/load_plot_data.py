2f059cb4d1e24cd49d41f2ba4f0e33875c25e314
inga420 <60630082+inga420@users.noreply.github.com>
2024-11-15 12:40:30 +0100

    added test results and python script to run verilog locally

--- python/load_plot_data.py
+++ python/load_plot_data.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-
+from vcd.reader import Token
 file_path = ''
 
 dataset  pd.read_csv(file_path, header=None)
@@ -11,3 +11,5 @@ data_frame = pd.DataFrame(dataset)
 data_array = np.array(data_frame.values)
 
 print(data_array.shape)
+
+plt.figure(figsize=(15,5))
